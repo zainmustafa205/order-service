f100a441dc95e8a50b14da5bdc414525b07598cf
Mirza Zain <zainmustafa205@gmail.com>
2026-07-09 23:17:39 +0500

    config: enable discovery client and feign clients in main application class

--- src/main/java/com/ecommerce/order_service/OrderServiceApplication.java
+++ src/main/java/com/ecommerce/order_service/OrderServiceApplication.java
@@ -2,8 +2,12 @@ package com.ecommerce.order_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
